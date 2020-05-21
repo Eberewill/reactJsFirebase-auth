cbaf1dcd76223b13676d1826155e731c4f73ecb8
Willex <eberewill@gmail.com>
2020-05-21 13:58:00 +0100

    signupworing

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -1,6 +1,6 @@
-import FirebaseContext from "./context";
 import Firebase from "./firebase";
+import FirebaseContext, { withFirebase } from "./context";
 
 export default Firebase;
 
-export { FirebaseContext };
+export { FirebaseContext, withFirebase };
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -9,6 +9,7 @@ const Navigation = () => (
       <li>
         <Link to={ROUTES.SIGN_IN}>Sign In</Link>
       </li>
+
       <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
       </li>
